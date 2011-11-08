28955fb838cfe57e8565c9a76f2b5c865704cff3
Dan Neame <danneame@gmail.com>
2011-11-08 20:36:23 +0000

    Map changes, stupid weapon added

--- js/src/Game.js
+++ js/src/Game.js
@@ -103,8 +103,9 @@
 				// todo - make Audio class
 				// todo - put this in the map data
 				nextTheme = function(){
-				  var playNext = themes.shift();
-				  jQuery( '#youtubecontainer' ).html( '<object width="1" height="1" style="position:absolute;left:-5000px;"><param name="movie" value="http://www.youtube.com/v/' + playNext + '?version=3&amp;hl=en_GB&amp;autoplay=1"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/' + playNext + '?version=3&amp;hl=en_GB&amp;autoplay=1" type="application/x-shockwave-flash" width="1" height="1" allowscriptaccess="always" allowfullscreen="true"></embed></object>' );
+					if( !themes.length ){ return; }
+				 	var playNext = themes.shift();
+				 	jQuery( '#youtubecontainer' ).html( '<object width="1" height="1" style="position:absolute;left:-5000px;"><param name="movie" value="http://www.youtube.com/v/' + playNext + '?version=3&amp;hl=en_GB&amp;autoplay=1"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/' + playNext + '?version=3&amp;hl=en_GB&amp;autoplay=1" type="application/x-shockwave-flash" width="1" height="1" allowscriptaccess="always" allowfullscreen="true"></embed></object>' );
 				},
 				/**
 				 * restoreThemes
